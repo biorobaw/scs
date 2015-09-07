1f6b1b5ad645a144b17ea25a95e672cc298cd59b
mllofriu <mllofriu@gmail.com>
2015-09-07 13:34:02 -0400

    running scripts

--- scripts/execOneCalib.sh
+++ scripts/execOneCalib.sh
@@ -5,20 +5,19 @@
 #SBATCH --mem=4000
 #SBATCH --cpus-per-task 2
 
-calibrationFile=$1
-logPath=$2
+logPath=$1
 if [ -z "$SLURM_ARRAY_TASK_ID" ]; then
   individual=$3
 else
   individual=$SLURM_ARRAY_TASK_ID
 fi
 
-if [ `hostname` == "pinky" ]; then
+if [ `whoami` == "biorob" ]; then
   export PATH=/work/R-3.1.1/bin:$PATH
   export R_LIBS=/work/R-3.1.1/library/
 else
   module add apps/R/3.1.2
   export R_LIBS=/home/m/mllofriualon/R-library/
 fi
-/usr/bin/java -cp "./platform/src/:./experiment/src/:./bin:./deps/*:./deps/j3dport/*" edu.usf.experiment.CalibrationExperiment $calibrationFile $logPath $individual
+/usr/bin/java -cp "./platform/src/:./experiment/src/:./bin:./deps/*:./deps/j3dport/*" edu.usf.experiment.CalibrationExperiment $logPath $individual
 
