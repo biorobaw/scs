9827e4f32cf91308e2c088403d72ebfb9f5d08c4
mllofriu <mllofriu@gmail.com>
2015-09-17 15:49:15 -0400

    remove dummy robot

--- scripts/execExperiment.sh
+++ scripts/execExperiment.sh
@@ -10,7 +10,7 @@ toIndiv=$4
 mkdir -p $logPath
 
 /usr/bin/java -cp "./platform/src/:./multiscalemodel/src/:./bin/:./deps/*:./deps/j3dport/*" edu.usf.experiment.PreExperiment $experimentFile $logPath > $logPath/preProc.txt 2>&1
-idMessage=`sbatch -a $fromIndiv-$toIndiv ./scripts/calibrationOneIndv.sh $logPath`
+idMessage=`sbatch -a $fromIndiv-$toIndiv ./scripts/execOne.sh $logPath`
 slurmId=`echo $idMessage | cut -d " " -f 4`
 sbatch --dependency=afterok:$slurmId scripts/postProcess.sh $logPath
 
