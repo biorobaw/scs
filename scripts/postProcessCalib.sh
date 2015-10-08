045517dd62cbda8d5ea02120fc4760aa42a49dc1
mllofriu <mllofriu@gmail.com>
2015-10-08 14:14:31 -0400

    post process scripts tuneup

--- scripts/postProcessCalib.sh
+++ scripts/postProcessCalib.sh
@@ -4,7 +4,10 @@
 
 logPath=$1
 
-module add apps/R/3.1.2
+if [ `whoami` == "mllofriualon" ]; then
+	module add apps/R/3.1.2
+	export R_LIBS=/home/m/mllofriualon/R-library/
+fi
 
 /usr/bin/java -cp "./platform/src/:./multiscalemodel/src/:./bin/:./deps/*:./deps/j3dport/*" edu.usf.experiment.CalibrationPostExperiment $logPath > $logPath/postProc.txt 2>&1
 
