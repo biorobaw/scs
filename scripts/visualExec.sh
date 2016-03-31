171103424a92553c01406e2101af831eab4059ca
Martin Llofriu <mllofriu@gmail.com>
2016-03-31 17:21:45 -0400

    execution scripts

--- scripts/visualExec.sh
+++ scripts/visualExec.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Example exec
+#  ./scripts/visualExec.sh "./multiscalemodel/src/edu/usf/ratsim/experiment/xml/tspSimpleRuns.xml" logs/Experiment/bat3 Control 0
 experimentFile=$1
 logPath=$2
 group=$3
