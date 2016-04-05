27890944ae62a7b93e31bac020f941b4a766194d
Martin Llofriu <mllofriu@gmail.com>
2016-04-05 09:39:49 -0400

    plots per episodes

--- platform/src/edu/usf/experiment/plot/plotRuntimesPerEpisode.r
+++ platform/src/edu/usf/experiment/plot/plotRuntimesPerEpisode.r
@@ -2,15 +2,15 @@ require(ggplot2, quietly = TRUE)
 require(plyr, quietly = TRUE)
 
 plotArrival <- function(pathData, plotName){
-  summarizedRunTimes <- ddply(pathData, .(trial, repetition), summarise, sdRT = sd(runtime)/sqrt(length(runtime)), mRT = mean(runtime))
-  summarizedRunTimes <- ddply(summarizedRunTimes, .(trial), summarise, repetition=repetition, mRT=mRT, sdRT=sdRT, runmedian = runmed(mRT, 31))
-  p <- ggplot(summarizedRunTimes, aes(x=repetition, y = mRT)) 
+  summarizedRunTimes <- ddply(pathData, .(trial, repetition, group), summarise, sdRT = sd(runtime)/sqrt(length(runtime)), mRT = mean(runtime))
+  summarizedRunTimes <- ddply(summarizedRunTimes, .(trial, group), summarise, repetition=repetition, mRT=mRT, sdRT=sdRT, runmedian = runmed(mRT, 31))
+  p <- ggplot(summarizedRunTimes, aes(x=repetition, y = mRT, color = group)) 
   p <- p + geom_errorbar(aes(ymin = mRT + sdRT, ymax=mRT - sdRT))
   p <- p + geom_line(aes(x=repetition, y=runmedian))
   p <- p + ylab("Num. of Steps") + xlab("Group") 
   p <- p + theme(legend.text = element_text(size=16), legend.title = element_text(size=16), text = element_text(size=16)) 
   p <- p + theme(legend.position = c(1, 1), legend.justification = c(1, 1), legend.background = element_rect(colour = NA, fill = NA))
-  ggsave(plot=p,filename=paste(plotName, ".", pathData[1,'group'],".pdf", sep=''), width=10, height=10)
+  ggsave(plot=p,filename=paste("runtimesPerEpisode.pdf", sep=''), width=10, height=10)
 }
 
 # Plot runtimes per episode
@@ -19,7 +19,7 @@ runtimeFrames<-lapply(files,function(x) {load(x); summarizedRunTimes['file'] <- 
 runtimes<-Reduce(function(x,y) merge (x,y, all=T), runtimeFrames)
 # names(runtimes)[2] <- "Group"
 #levels(runtimes$Group) <- c("Multi-Scale (3 Layers)", "Small Scale (1 Layer)", "Medium Scale (1 Layer)", "Large Scale (1 Layer)")
-ddply(runtimes, .(group), function(x) plotArrival(x, plotName="runtimes"))
+plotArrival(runtimes, plotName="runtimes")
 
  
 
