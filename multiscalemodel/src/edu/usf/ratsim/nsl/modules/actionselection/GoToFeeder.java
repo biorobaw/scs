6a145ce03c30d047521c4cc60be48152c77a71c0
Martin Llofriu <mllofriu@gmail.com>
2016-09-15 16:00:37 -0400

    Go to the most voted feeder

--- multiscalemodel/src/edu/usf/ratsim/nsl/modules/actionselection/GoToFeeder.java
+++ multiscalemodel/src/edu/usf/ratsim/nsl/modules/actionselection/GoToFeeder.java
@@ -6,55 +6,70 @@ import java.util.List;
 import java.util.Random;
 
 import edu.usf.experiment.robot.LocalizableRobot;
+import edu.usf.experiment.subject.Subject;
+import edu.usf.experiment.subject.affordance.EatAffordance;
 import edu.usf.experiment.universe.Feeder;
 import edu.usf.experiment.utils.GeomUtils;
 import edu.usf.micronsl.module.Module;
 import edu.usf.micronsl.port.onedimensional.Float1dPort;
+import edu.usf.micronsl.port.singlevalue.Int0dPort;
 
 public class GoToFeeder extends Module {
 
 	private LocalizableRobot lRobot;
 	private Random random;
+	private Int0dPort takenActionPort;
+	private Subject sub;
 
-	public GoToFeeder(String name, List<Integer> feederOrder,
-			LocalizableRobot lRobot, Random random) {
+	public GoToFeeder(String name, Subject sub, Random random) {
 		super(name);
 
-		this.lRobot = lRobot;
+		this.lRobot = (LocalizableRobot) sub.getRobot();
+		this.sub = sub;
 		this.random = random;
+		
+		takenActionPort = new Int0dPort(this);
+		addOutPort("takenAction", takenActionPort);
 	}
 
 	@Override
 	public void run() {
 		Float1dPort votes = (Float1dPort) getInPort("votes");
 
-		List<Feeder> allFeeders = lRobot.getVisibleFeeders(null);
+		List<Feeder> visibleFeeders = lRobot.getVisibleFeeders(null);
 
 		// Get action value for each visible feeder
 		List<ActionValue> avList = new LinkedList<ActionValue>();
-		for (Feeder f : allFeeders)
-			avList.add(new ActionValue(f.getId(), votes.get(f.getId())));
+		for (Feeder f : visibleFeeders)
+			// Votes at 0 are for feeder 1
+			avList.add(new ActionValue(f.getId(), votes.get(f.getId() - 1)));
 		// Sort avList by value
 		Collections.sort(avList);
+		
+		// Check whether the robot is seeing the currently seek feeder
+		boolean seeToGoFeeder = false;
+		if (!avList.isEmpty()){
+			for (Feeder f : visibleFeeders){
+				seeToGoFeeder |= f.getId() == avList.get(0).getAction();
+			}
+		}
 
-		if (avList.isEmpty()) {
+		// If not seeing the seek feeder
+		if (!seeToGoFeeder) {
 			// Turn 120
 			if (random.nextBoolean())
 				lRobot.rotate(120);
 			else
 				lRobot.rotate(-120);
+			takenActionPort.set(-1);
 		} else {
-			// If desired feeder close
-			// Eat
-			// Else
-			// Go to most voted feeder
 			int desiredFeederId = avList.get(0).getAction();
 			if (lRobot.isFeederClose()
-					&& lRobot.getClosestFeeder().getId() == desiredFeederId)
-				lRobot.eat();
-			else {
+					&& lRobot.getClosestFeeder().getId() == desiredFeederId){
+				lRobot.executeAffordance(new EatAffordance(), sub);
+			} else {
 				Feeder desiredFeeder = null;
-				for (Feeder f : allFeeders)
+				for (Feeder f : visibleFeeders)
 					if (f.getId() == desiredFeederId)
 						desiredFeeder = f;
 				// Todo: make it a step
@@ -63,6 +78,8 @@ public class GoToFeeder extends Module {
 				// Parametrize
 				lRobot.forward(.1f);
 			}
+			
+			takenActionPort.set(desiredFeederId);
 
 		}
 
@@ -70,7 +87,7 @@ public class GoToFeeder extends Module {
 
 	@Override
 	public boolean usesRandom() {
-		return false;
+		return true;
 	}
 
 }
