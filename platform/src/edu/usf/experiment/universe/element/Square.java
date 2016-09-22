58cf1beb436d168f7b8b1b20f13f7b95005dc7d5
Martin Llofriu <mllofriu@gmail.com>
2016-09-22 13:53:18 -0400

    added missing walls, fix place cell layout, decrease neg reward, added decay exploration gain. Changed task to always timeout flash. It seems to learn now, converging in the no obs task

---
+++ platform/src/edu/usf/experiment/universe/element/Square.java
@@ -0,0 +1,26 @@
+package edu.usf.experiment.universe.element;
+
+import edu.usf.experiment.universe.Wall;
+import edu.usf.experiment.utils.ElementWrapper;
+
+public class Square extends MazeElement {
+	
+	public float l,x,y;
+
+	public Square(ElementWrapper params) {
+		super(params);
+		// TODO Auto-generated constructor stub
+		
+		if (params != null){
+			l = params.getChildFloat("l");
+			x = params.getChildFloat("x");
+			y = params.getChildFloat("y");
+				
+			walls.add(new Wall(x-l/2, y-l/2, x-l/2, y+l/2));
+			walls.add(new Wall(x-l/2, y-l/2, x+l/2, y-l/2));
+			walls.add(new Wall(x+l/2, y+l/2, x-l/2, y+l/2));
+			walls.add(new Wall(x+l/2, y+l/2, x+l/2, y-l/2));
+		}
+	}
+
+}
