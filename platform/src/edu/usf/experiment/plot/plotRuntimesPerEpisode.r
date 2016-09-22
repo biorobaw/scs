8fc83da909dfb28b7481cbc4bdf0734629d5d49b
Martin Llofriu <mllofriu@gmail.com>
2016-09-22 14:58:24 -0400

    Changed runtime plot per episode. Fix deactivation in the delayed cue without obstacles.

--- platform/src/edu/usf/experiment/plot/plotRuntimesPerEpisode.r
+++ platform/src/edu/usf/experiment/plot/plotRuntimesPerEpisode.r
@@ -1,25 +1,52 @@
 require(ggplot2, quietly = TRUE)
+require(grid)
 require(plyr, quietly = TRUE)
+require(dunn.test, quietly = TRUE)
+
+stepsPerSec <- 4
 
 plotArrival <- function(pathData, plotName){
-  summarizedRunTimes <- ddply(pathData, .(trial, repetition, group), summarise, sdRT = sd(runtime)/sqrt(length(runtime)), mRT = mean(runtime))
-  summarizedRunTimes <- ddply(summarizedRunTimes, .(trial, group), summarise, repetition=repetition, mRT=mRT, sdRT=sdRT, runmedian = runmed(mRT, 31))
-  p <- ggplot(summarizedRunTimes, aes(x=repetition, y = mRT, color = group)) 
-  p <- p + geom_errorbar(aes(ymin = mRT + sdRT, ymax=mRT - sdRT))
-  p <- p + geom_line(aes(x=repetition, y=runmedian))
-  p <- p + ylab("Num. of Steps") + xlab("Group") 
-  p <- p + theme(legend.text = element_text(size=16), legend.title = element_text(size=16), text = element_text(size=16)) 
-  p <- p + theme(legend.position = c(1, 1), legend.justification = c(1, 1), legend.background = element_rect(colour = NA, fill = NA))
-  ggsave(plot=p,filename=paste("runtimesPerEpisode.pdf", sep=''), width=10, height=10)
+  #pathData <- pathData[pathData$runtime < 10000,]
+  pathData$runtime <- pathData$runtime / stepsPerSec
+  summarizedRunTimes <- ddply(pathData, .(group, repetition), summarise, sdRT = sd(runtime)/sqrt(length(runtime)), mRT = mean(runtime))
+  #   print(head(summarizedRunTimes))
+  summarizedRunTimes <- ddply(summarizedRunTimes, .(group), summarise, repetition=repetition, mRT=mRT, sdRT=sdRT, runmedian = runmed(mRT, 31))
+  #   print(pathData[1,'trial'])
+  p <- ggplot(pathData, aes(x=factor(repetition), y = runtime)) 
+  #p <- p + geom_bar(data=summarizedRunTimes, mapping=aes(x=Group, y = mRT), stat='identity')
+  p <- p + ylab("Completion time (s)\n") + xlab("\nGroup") 
+  # p <- p + scale_fill_grey(name="Group",start = .4)
+  p <- p + theme_bw() + scale_fill_discrete(guide = guide_legend(title = "Group"))
+  p <- p + scale_y_continuous()
+  p <- p + theme(legend.text = element_text(size=20), legend.title = element_text(size=20), text = element_text(size=20)) 
+  p <- p + theme(legend.key.height = unit(3,"line"), legend.key.width = unit(3,"line"), legend.position = "right", legend.justification = c(1, 1), legend.background = element_rect(colour = NA, fill = NA))
+  box <- p + geom_boxplot(aes(fill=group),position=position_dodge(1), notch=TRUE)# + geom_jitter()
+  print(box)
+  ggsave(plot=box,filename=paste(plotName, "box.", pathData[1,'trial'],".pdf", sep=''), width=10, height=10)
+  
+  p <- ggplot(summarizedRunTimes, aes(x=group, y = mRT)) 
+  p <- p + ylab("Completion time (s)") + xlab("Group") 
+  p <- p + theme(legend.text = element_text(size=20), legend.title = element_text(size=20), text = element_text(size=20)) 
+  p <- p + theme(legend.key.height = unit(3,"line"), legend.key.width = unit(3,"line"), legend.position = "right", legend.justification = c(1, 1), legend.background = element_rect(colour = NA, fill = NA))
+  p <- p + geom_bar(aes(y=mRT,fill=group),position=position_dodge(1), stat="identity")
+  p <- p + geom_errorbar(aes(x=group, ymax=mRT+sdRT, ymin=mRT-sdRT, color=group, width = 0.25))
+  p <- p + scale_fill_grey(name="Group")
+  p <- p + scale_color_grey(name="Group")
+  ggsave(plot=p,filename=paste(plotName, "bar.", pathData[1,'trial'],".pdf", sep=''), width=10, height=10)
 }
 
 # Plot runtimes per episode
 files <- list.files('.', 'summary.RData', recursive=T)
 runtimeFrames<-lapply(files,function(x) {load(x); summarizedRunTimes['file'] <- x; summarizedRunTimes})
 runtimes<-Reduce(function(x,y) merge (x,y, all=T), runtimeFrames)
-# names(runtimes)[2] <- "Group"
-#levels(runtimes$Group) <- c("Multi-Scale (3 Layers)", "Small Scale (1 Layer)", "Medium Scale (1 Layer)", "Large Scale (1 Layer)")
-plotArrival(runtimes, plotName="runtimes")
+save(runtimes, file="runtimes.RData")
+load('runtimes.RData')
+# runtimes <- runtimes[runtimes$runtime != 200000,]
+ddply(runtimes, .(trial), function(x) plotArrival(x, plotName="runtimesEpisode"))
 
- 
-
+c <- runtimes[runtimes$trial == "DelayedCueObs" & runtimes$group == "Control", "runtime"]
+d <- runtimes[runtimes$trial == "DelayedCueObs" & runtimes$group == "Dorsal", "runtime"]
+v <- runtimes[runtimes$trial == "DelayedCueObs" & runtimes$group == "Ventral", "runtime"]
+x <- c(c, d, v)
+g <- factor(rep(1:3, c(length(c), length(d), length(v))), labels=c("Control", "Dorsal", "Ventral"))
+dunn.test(x,g)
