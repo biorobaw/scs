4d7ee09adbbc0a06bbb5d8ef447f2a9916b01871
Martin Llofriu <mllofriu@gmail.com>
2016-10-03 17:15:15 -0400

    Slam state proxy

---
+++ multiscalemodel/src/edu/usf/ratsim/robot/ssl/slam/SlamStateProxy.java
@@ -0,0 +1,114 @@
+package edu.usf.ratsim.robot.ssl.slam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.vecmath.Point3f;
+
+import edu.usf.ratsim.robot.ssl.slam.protobuf.Slamstate.SlamState;
+
+public class SlamStateProxy extends Thread {
+
+	private static final String HOST = "locahost";
+	private static final int PORT = 12346;
+	private Socket protoSocket;
+	private boolean terminated;
+	private InputStream protoInputStream;
+	private ServerSocket serverSocket;
+
+	private SlamState ss;
+
+	public SlamStateProxy() {
+		establishConnection();
+
+		ss = SlamState.getDefaultInstance();
+
+		terminated = false;
+	}
+
+	private void establishConnection() {
+		try {
+			if (protoSocket != null)
+				protoSocket.close();
+
+			if (serverSocket != null)
+				serverSocket.close();
+
+			System.out.println("Listening to " + PORT);
+			serverSocket = new ServerSocket(PORT);
+			protoSocket = serverSocket.accept();
+			protoInputStream = protoSocket.getInputStream();
+			System.out.println("Connection stablished");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		while (!terminated) {
+			try {
+				SlamState newSS = SlamState.parseDelimitedFrom(protoInputStream);
+				if (newSS == null) {
+					setSlamState(SlamState.getDefaultInstance());
+					establishConnection();
+				} else {
+					setSlamState(newSS);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private synchronized void setSlamState(SlamState ss) {
+		this.ss = ss;
+	}
+
+	private synchronized int getTrackingState(){
+		return ss.getState();
+	}
+
+	private synchronized Point3f getPosition(){
+		return new Point3f(ss.getX(), ss.getY(), 0);
+	}
+
+	private synchronized float getOrientation(){
+		return ss.getHeading();
+	}
+
+	public static void main(String[] args) {
+		SlamStateProxy ssProxy = new SlamStateProxy();
+		ssProxy.start();
+
+		while (true) {
+			int state = ssProxy.getTrackingState();
+			System.out.println("Tracking state " + state);
+			Point3f p = ssProxy.getPosition();
+			System.out.println("Robot at " + p);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void close() {
+		try {
+			protoInputStream.close();
+			serverSocket.close();
+			protoSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
