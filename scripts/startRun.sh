2004541a4ab93f33e02712473b141095e6ac1ad8
Martin Llofriu <mllofriu@gmail.com>
2016-10-07 15:22:45 -0400

    launch tune up

--- scripts/startRun.sh
+++ scripts/startRun.sh
@@ -5,9 +5,16 @@ logs=$2
 group=$3
 individual=$4
 
+roslaunch orb_slam2 orb_slam.launch &
+
+sleep 10
+
 java -Dgnu.io.rxtx.SerialPorts=/dev/teensy:/dev/uno \
 	-Djava.library.path=/usr/lib/jni/ -classpath    \
 	"./bin/:./deps/*" edu.usf.experiment.Experiment $xml $logs $group $individual &
 
-sleep 2	
-roslaunch scripts/model.launch
+sleep 2
+
+roslaunch ssl_driver ssl_driver.launch
+
+#roslaunch multiscalemodel/src/edu/usf/ratsim/robot/ssl/model.launch
