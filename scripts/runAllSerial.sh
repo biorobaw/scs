0704e3e288eb1403f32eb778088a2bf0ff2134a9
Martin Llofriu <mllofriu@gmail.com>
2016-10-13 16:02:05 -0400

    smarter run serial

--- scripts/runAllSerial.sh
+++ scripts/runAllSerial.sh
@@ -2,10 +2,13 @@
 
 xml=$1
 logs=$2
+indivPerGroup=$3
 
 truncate --size 0 out
 
-./scripts/execExperimentSerial.sh $xml $logs 0 31 >> out &
-./scripts/execExperimentSerial.sh $xml $logs 32 63 >> out & 
-./scripts/execExperimentSerial.sh $xml $logs 64 95 >> out &
-./scripts/execExperimentSerial.sh $xml $logs 96 127 >> out &
+groupIni=0
+for i in $(seq 1 4); do
+    groupEnd=$(($groupIni+$indivPerGroup-1))
+    ./scripts/execExperimentSerial.sh $xml $logs $groupIni $groupEnd  >> out &
+    groupIni=$(($groupEnd+1))
+done
