074211efe467c54a78b9bb9b02a646a22fb8535b
Martin Llofriu <mllofriu@gmail.com>
2016-11-06 16:10:46 -0500

    Simplified the model

--- multiscalemodel/src/edu/usf/ratsim/experiment/subject/multipleT/MultipleTSubject.java
+++ multiscalemodel/src/edu/usf/ratsim/experiment/subject/multipleT/MultipleTSubject.java
@@ -14,19 +14,10 @@ import edu.usf.experiment.robot.LocalizableRobot;
 import edu.usf.experiment.robot.Robot;
 import edu.usf.experiment.subject.Subject;
 import edu.usf.experiment.subject.affordance.Affordance;
-import edu.usf.experiment.subject.affordance.EatAffordance;
-import edu.usf.experiment.subject.affordance.ForwardAffordance;
-import edu.usf.experiment.subject.affordance.TurnAffordance;
 import edu.usf.experiment.utils.BinaryFile;
 import edu.usf.experiment.utils.CSVReader;
 import edu.usf.experiment.utils.ElementWrapper;
-import edu.usf.micronsl.Model;
-import edu.usf.micronsl.Datatypes.SparseMatrix;
-import edu.usf.micronsl.port.onedimensional.sparse.Float1dSparsePort;
-import edu.usf.micronsl.port.onedimensional.sparse.Float1dSparsePortMap;
-import edu.usf.micronsl.port.singlevalue.Float0dPort;
 import edu.usf.ratsim.experiment.subject.NotImplementedException;
-import edu.usf.ratsim.experiment.universe.virtual.VirtUniverse;
 import edu.usf.ratsim.nsl.modules.cell.PlaceCell;
 import edu.usf.ratsim.robot.virtual.VirtualRobot;
 
@@ -34,20 +25,8 @@ public class MultipleTSubject extends Subject {
 
 	public float step;
 
-	// private float leftAngle;
-	// private float rightAngle;
-	public enum State {
-		AWAKE, ASLEEP
-	};
-
-	public State state = State.AWAKE;
-
-	private MultipleTModel model;
 	private MultipleTModelAsleep modelAsleep;
 	private MultipleTModelAwake modelAwake;
-
-	// public SparseMatrix<Float> QTable;
-	// public SparseMatrix<Float> WTable;
 
 	public float[][] QTable;
 	public float[][] WTable;
@@ -55,15 +34,9 @@ public class MultipleTSubject extends Subject {
 	int numActions;
 	int numPC;
 
-	int laps;
-	int noFoodLaps;
 	int cantReplay;
 	int episodesPerSession;
 	float replayThres;
-
-	public int iteration;
-	public int episode = 0;
-	public int trial = -1;
 
 	public VirtualRobot lRobot;
 	public float awakeFoodDistanceThreshold;
@@ -88,8 +61,6 @@ public class MultipleTSubject extends Subject {
 
 		replayThres = params.getChildFloat("replayThres");
 		cantReplay = params.getChildInt("cantReplay");
-		laps = params.getChildInt("laps");
-		noFoodLaps = params.getChildInt("noFoodLaps");
 
 		awakeFoodDistanceThreshold = params.getChildFloat("awakeFoodDistanceThreshold");
 		asleepFoodDistanceThreshold = params.getChildFloat("asleepFoodDistanceThreshold");
@@ -99,7 +70,6 @@ public class MultipleTSubject extends Subject {
 
 		Integer loadEpisode = (Integer) g.get("loadEpisode");
 		if (loadEpisode != null) {
-			episode = loadEpisode;
 			String loadPath = (String) g.get("logPath") + "/" + (String) g.get("loadTrial") + "/" + loadEpisode + "/"
 					+ (String) g.get("groupName") + "/" + g.get("subName") + "/";
 
@@ -135,28 +105,6 @@ public class MultipleTSubject extends Subject {
 
 		modelAsleep = new MultipleTModelAsleep(params, this, lRobot, numActions, numPC,
 				(LinkedList<PlaceCell>) modelAwake.placeCells.getCells());
-		model = modelAwake;
-
-		iteration = 0;
-	}
-
-	public void swapState() {
-		if (state == State.AWAKE)
-			setAsleep();
-		else
-			setAwake();
-	}
-
-	public void setAwake() {
-		state = State.AWAKE;
-		model = modelAwake;
-		lRobot.setCloseThreshold(awakeFoodDistanceThreshold);
-	}
-
-	public void setAsleep() {
-		state = State.ASLEEP;
-		model = modelAsleep;
-		lRobot.setCloseThreshold(asleepFoodDistanceThreshold);
 	}
 
 	public void save() {
@@ -168,33 +116,13 @@ public class MultipleTSubject extends Subject {
 
 		PrintWriter writer;
 		try {
-			// save WTable if not asleep:
-			if (state != State.ASLEEP) {
-				String filename = logPath + "WTable.bin";
-				BinaryFile.saveSparseBinaryMatrix(WTable, filename);
-
-				// writer = new PrintWriter(logPath + "WTable.txt", "UTF-8");
-				// for(int i=0;i<numPC;i++){
-				// for(int j=0;j<numPC;j++){
-				// writer.print(WTable[i][j]+";");
-				// }
-				// writer.println();
-				// }
-				// writer.close();
-			}
+			// Save transition table
+			String filename = logPath + "WTable.bin";
+			BinaryFile.saveSparseBinaryMatrix(WTable, filename);
 
 			// save QTable
-			String filename = logPath + "QTable.bin";
+			filename = logPath + "QTable.bin";
 			BinaryFile.saveBinaryMatrix(QTable, filename);
-
-			// writer = new PrintWriter(logPath + "QTable.txt", "UTF-8");
-			// for(int i=0;i<numPC;i++){
-			// for(int j=0;j<numActions;j++){
-			// writer.print(QTable[i][j]+";");
-			// }
-			// writer.println();
-			// }
-			// writer.close();
 
 			// save cells
 			writer = new PrintWriter(logPath + "cellCenters.txt", "UTF-8");
@@ -215,50 +143,33 @@ public class MultipleTSubject extends Subject {
 
 	@Override
 	public void stepCycle() {
-
-		iteration++;
-		// System.out.println("iteration: "+iteration);
-		// if( i==642) new java.util.Scanner(System.in).nextLine();
-
 		setHasEaten(false);
 		clearTriedToEAt();
 
-		model.simRun();
+		modelAwake.simRun();
 
 		// Replay after eating
 		// But first wait one extra cycle
 		if (hasEaten() && !waitingOne) 
 			waitingOne = true;
 		else if (waitingOne){
+			// Now we are ready to execute replay
 			waitingOne = false;
 			lRobot.setCloseThreshold(asleepFoodDistanceThreshold);
 
+			// Execute replay episodes
 			for (int r = 0; r < cantReplay; r++) {
-//				System.out.println("Replay event " + r);
 				modelAsleep.newEpisode();
 				setHasEaten(false);
 				clearTriedToEAt();
 				do {
 					modelAsleep.simRun();
-
 				} while (!hasEaten() && modelAsleep.getMaxActivation() > replayThres);
 
 			}
 
 			lRobot.setCloseThreshold(awakeFoodDistanceThreshold);
 		}
-
-		// MultipleTModelAwake awake = (MultipleTModelAwake)model;
-		// System.out.println("Q: "+awake.currentStateQ.voteString());
-		// Float1dSparsePortMap pcs =
-		// (Float1dSparsePortMap)awake.placeCells.getOutPort("activation");
-		// if( episode==2 && pcs.get(37)!=0) {
-		//
-		// System.out.println("MODEL Q:");
-		// for(int i=0;i<8;i++) System.out.print(QTable[37][i]+" ");
-		//
-		// new java.util.Scanner(System.in).nextLine();
-		// }
 	}
 
 	@Override
@@ -274,60 +185,14 @@ public class MultipleTSubject extends Subject {
 
 	@Override
 	public void newEpisode() {
-//		Globals g = Globals.getInstance();
-//		int e = (int) g.get("episode");
-//		VirtUniverse u = VirtUniverse.getInstance();
-//
-//		// noFoodLaps = 0;
-//		// cantReplay = 10;
-//
-//		// food activation:
-//		if (e < noFoodLaps) {
-//			for (Integer f : u.getEnabledFeeders()) {
-//				u.setActiveFeeder(f, false);
-//				u.clearFoodFromFeeder(f);
-//			}
-//			setAwake(); // no need to do replay since it wont have any effect
-//		} else {
-//			for (Integer f : u.getEnabledFeeders())
-//				u.setActiveFeeder(f, true);
-//
-//			if ((e - noFoodLaps) % (cantReplay + 1) == 0) {// do 1 awake 10
-//															// asleep
-//				setAwake();
-//
-//			} else {
-//				setAsleep();
-//			}
-//
-//		}
-//
-//		// RESET TRANSITION MATRIX
-//		// if(e % 20 == 0) //reset transition matrix every episodesPerSession
-//		// {
-//		// for (int i =0;i<numPC;i++)
-//		// for(int j = 0; j<numPC;j++)
-//		// WTable[i][j] = 0.0f;
-//		// for (int i =0;i<numPC;i++)
-//		// for(int j = 0; j<numActions;j++)
-//		// QTable[i][j] = 0.0f;
-//		// }
-
-		setAwake();
-		model.newEpisode();
+		modelAwake.newEpisode();
 		waitingOne = false;
-//		iteration = 0;
-//		episode++;
-//		System.out.println("T,E: " + trial + " " + e);
-//		System.out.println("State: " + state);
 	}
 
 	@Override
 	public void newTrial() {
-		model.newTrial();
-		iteration = 0;
-		trial++;
-		episode = 0;
+		modelAwake.newTrial();
+		modelAsleep.newTrial();
 	}
 
 	@Override
@@ -356,7 +221,7 @@ public class MultipleTSubject extends Subject {
 	}
 
 	public List<PlaceCell> getPlaceCells() {
-		return model.getPlaceCells();
+		return modelAwake.getPlaceCells();
 	}
 
 	@Override
@@ -370,7 +235,7 @@ public class MultipleTSubject extends Subject {
 	}
 
 	public Map<Integer, Float> getPCActivity() {
-		return model.getCellActivation();
+		return modelAwake.getCellActivation();
 	}
 
 	@Override
@@ -381,20 +246,6 @@ public class MultipleTSubject extends Subject {
 	@Override
 	public void reactivateHPCLayers(LinkedList<Integer> indexList) {
 		throw new NotImplementedException();
-	}
-
-	public boolean replayAboveThreshold() {
-		if (state == State.AWAKE)
-			return true;
-		// System.out.println("max activation: "+model.getMaxActivation());
-		return model.getMaxActivation() > replayThres;
-	}
-
-	public boolean loopInReactivationPath() {
-		if (state == State.AWAKE)
-			return false;
-		// System.out.println("max activation: "+model.getMaxActivation());
-		return ((MultipleTModelAsleep) model).loopInReactivationPath();
 	}
 
 }
