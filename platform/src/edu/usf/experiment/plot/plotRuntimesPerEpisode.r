b3a52de56becf9a7efe7a7d6104792b94dc2b608
Martin Llofriu <mllofriu@gmail.com>
2016-12-20 18:30:46 -0500

    fix runtime per episode plot for many groups

--- platform/src/edu/usf/experiment/plot/plotRuntimesPerEpisode.r
+++ platform/src/edu/usf/experiment/plot/plotRuntimesPerEpisode.r
@@ -2,6 +2,7 @@ require(ggplot2, quietly = TRUE)
 require(grid)
 require(plyr, quietly = TRUE)
 require(dunn.test, quietly = TRUE)
+require(scales, quietly = TRUE)
 
 #stepsPerSec <- 4
 
@@ -11,15 +12,15 @@ plotArrival <- function(pathData, plotName){
   #   print(head(summarizedRunTimes))
   summarizedRunTimes <- ddply(summarizedRunTimes, .(group), summarise, repetition=repetition, mRT=mRT, sdRT=sdRT, runmedian = runmed(mRT, 31))
   #   print(pathData[1,'trial'])
-  p <- ggplot(pathData, aes(x=repetition, y = runtime, group = factor(repetition))) 
+  p <- ggplot(pathData, aes(x = repetition, y = runtime, group = interaction(group, repetition), fill = group)) 
   #p <- p + geom_bar(data=summarizedRunTimes, mapping=aes(x=Group, y = mRT), stat='identity')
   p <- p + ylab("Completion time (num. actions)\n") + xlab("\nRepetition") 
   # p <- p + scale_fill_grey(name="Group",start = .4)
   p <- p + theme_bw() + scale_fill_discrete(guide = guide_legend(title = "Group"))
-  p <- p + scale_y_continuous()
+  p <- p + scale_y_continuous() + scale_x_continuous(breaks= pretty_breaks())
   p <- p + theme(legend.text = element_text(size=20), legend.title = element_text(size=20), text = element_text(size=20)) 
   p <- p + theme(legend.key.height = unit(3,"line"), legend.key.width = unit(3,"line"), legend.position = "right", legend.justification = c(1, 1), legend.background = element_rect(colour = NA, fill = NA))
-  box <- p + geom_boxplot(aes(fill=group),position="dodge")# + geom_jitter()
+  box <- p + geom_boxplot(position="dodge")# + geom_jitter()
   #box <- p + geom_point(aes(color=group), size = 5)
   print(box)
   ggsave(plot=box,filename=paste(plotName, "box.", pathData[1,'trial'],".pdf", sep=''), width=30, height=10)
@@ -36,10 +37,10 @@ plotArrival <- function(pathData, plotName){
 }
 
 # Plot runtimes per episode
-files <- list.files('.', 'summary.RData', recursive=T)
-runtimeFrames<-lapply(files,function(x) {load(x); summarizedRunTimes['file'] <- x; summarizedRunTimes})
-runtimes<-Reduce(function(x,y) merge (x,y, all=T), runtimeFrames)
-save(runtimes, file="runtimes.RData")
+# files <- list.files('.', 'summary.RData', recursive=T)
+# runtimeFrames<-lapply(files,function(x) {load(x); summarizedRunTimes['file'] <- x; summarizedRunTimes})
+# runtimes<-Reduce(function(x,y) merge (x,y, all=T), runtimeFrames)
+# save(runtimes, file="runtimes.RData")
 load('runtimes.RData')
 # runtimes <- runtimes[runtimes$runtime != 200000,]
 ddply(runtimes, .(trial), function(x) plotArrival(x, plotName="runtimesEpisode"))
