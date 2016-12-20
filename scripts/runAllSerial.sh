718b616459a6a6c059f1f0ba24ec45f283c817fc
Martin Llofriu <mllofriu@gmail.com>
2016-12-20 18:30:56 -0500

    fix run all serial

--- scripts/runAllSerial.sh
+++ scripts/runAllSerial.sh
@@ -2,21 +2,13 @@
 
 xml=$1
 logs=$2
-<<<<<<< HEAD
-indivPerGroup=$3
-=======
 groups=$3
 indivPerGroup=$4
->>>>>>> multipleTBranch
 
 truncate --size 0 out
 
 groupIni=0
-<<<<<<< HEAD
-for i in $(seq 1 4); do
-=======
 for i in $(seq 1 $groups); do
->>>>>>> multipleTBranch
     groupEnd=$(($groupIni+$indivPerGroup-1))
     ./scripts/execExperimentSerial.sh $xml $logs $groupIni $groupEnd  >> out &
     groupIni=$(($groupEnd+1))
