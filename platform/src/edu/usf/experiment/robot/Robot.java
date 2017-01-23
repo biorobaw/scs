3fc35a11d30bc9089eec7c27945a0422b85e886e
Pablo Scleidorovich <bucefalog@gmail.com>
2017-01-22 20:28:07 -0500

    Previous command had to be broken. This is concludes he commit (which started 2 comits ago).

---
+++ platform/src/edu/usf/experiment/robot/Robot.java
@@ -0,0 +1,22 @@
+package edu.usf.experiment.robot;
+
+import java.util.LinkedList;
+
+
+public abstract class Robot {
+	
+	public LinkedList<RobotAction> pendingActions = new LinkedList<RobotAction>();
+
+	public void processPendingActions(){
+		for(RobotAction a : pendingActions)
+			processAction(a);
+		pendingActions.clear();
+	}
+
+	public abstract void processAction(RobotAction action);
+	
+	public abstract void executeTimeStep(float deltaT);
+	
+}
+
+
---
+++ platform/src/edu/usf/experiment/subject/Subject.java
@@ -0,0 +1,81 @@
+package edu.usf.experiment.subject;
+
+import edu.usf.experiment.robot.Robot;
+import edu.usf.experiment.utils.ElementWrapper;
+
+public abstract class Subject {
+	protected String name;
+	protected String group;
+	protected Robot robot;
+	protected boolean hasEaten;
+	
+	public Subject(){};
+	
+	public Subject(String name, String group, ElementWrapper modelParams,Robot robot) {
+		this.name = name;
+		this.group = group;
+		this.robot = robot;
+		System.out.println("group: " + group);
+		hasEaten = false;
+
+	}
+	
+	public void save(){};
+	
+	/**
+	 * Returns the name of the subject
+	 * 
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+	
+	/**
+	 * Returns whether the subject has eaten in the last iteration
+	 * @return
+	 */
+	public boolean hasEaten(){
+		return hasEaten;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public void setName(String subjectName) {
+		this.name = subjectName;
+	}
+	
+	public Robot getRobot() {
+		return robot;
+	}
+	
+	public void setHasEaten(boolean b) {
+		hasEaten = b;
+	}
+	
+	
+	
+	/**
+	 * Advances one cycle in the internal model of the brain usually resulting
+	 * in a decision being taken
+	 */
+	public abstract void stepCycle();
+	
+	public abstract void newEpisode();
+	
+	public abstract void newTrial();
+	
+	public void endEpisode(){}
+	
+	
+	
+	
+	
+
+}
