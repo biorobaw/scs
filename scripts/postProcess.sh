655a6caee912062027a2c541267d34453042544f
mllofriu <mllofriu@gmail.com>
2017-03-22 16:36:39 -0400

    fix for new circe

--- scripts/postProcess.sh
+++ scripts/postProcess.sh
@@ -4,9 +4,11 @@
 logPath=$1
 
 if [ `whoami` == "mllofriualon" ]; then
-    module add apps/R/3.1.2
-    export R_LIBS=/home/m/mllofriualon/R-library/
+  module add apps/R/3.3.2
+  export R_LIBS=/home/m/mllofriualon/work/rlib
+  module add apps/jre/1.8.0_121.x86  
+  module unload apps/jre/1.7.0_80.x64
 fi
 
-/usr/bin/java -cp "./platform/src/:./multiscalemodel/src/:./bin/:./deps/*:./deps/j3dport/*" edu.usf.experiment.PostExperiment $logPath > $logPath/postProc.txt 2>&1
+java -cp "./platform/src/:./multiscalemodel/src/:./bin/:./deps/*:./deps/j3dport/*" edu.usf.experiment.PostExperiment $logPath > $logPath/postProc.txt 2>&1
 
