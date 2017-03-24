9057a26f2415f59055a966d26b7db1614b9cc886
Pablo Scleidorovich <bucefalog@gmail.com>
2017-03-24 14:37:12 -0400

    starting to execute in cluster

---
+++ Batteries/MultipleTremoving/removing.bat
@@ -0,0 +1,31 @@
+constant SOURCE_FOLDERS
+"./platform/src/;./multiscalemodel/src/;./bin/;./deps/*;./deps/j3dport/*"
+
+constant MAIN_CLASS
+edu.usf.experiment.Experiment
+
+constant EXPERIMENT_XML
+"./Batteries/MultipleTremoving/xml/multipleTexperimentRemoving.xml"
+
+variable LOG_FOLDER 
+logs/3-21-2017/MultipleTNoFood10Laps40Replay200Thres10RemovingAtFood/
+
+
+variable GROUP 
+Control
+Removing
+#NoHab
+#NoReplay
+
+variable RAT_NUMBER
+0
+1
+2
+3
+4
+5
+6
+7
+
+
+combine allXall
---
+++ Batteries/sampleBattery.bat
@@ -0,0 +1,25 @@
+constant SOURCE_FOLDERS
+"./platform/src/;./multiscalemodel/src/;./bin/;./deps/*;./deps/j3dport/*"
+
+constant MAIN_CLASS
+edu.usf.experiment.Experiment
+
+constant EXPERIMENT_XML
+"./multiscalemodel/src/edu/usf/ratsim/experiment/xml/tspSimpleRunsFullPath.xml"
+
+variable LOG_FOLDER 
+myLogFolder
+
+constant GROUP 
+Control
+
+constant RAT_NUMBER
+1
+
+constant ratpath
+Inter/1
+
+constant enabledFeeders 
+18,21,3,5
+
+combine allXall
