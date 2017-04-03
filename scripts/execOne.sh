deed387197d1f3d477458ab52d936dfeabdb0c6e
Pablo Scleidorovich <bucefalog@gmail.com>
2017-04-03 17:10:26 -0400

    updated scripts

--- scripts/execOne.sh
+++ scripts/execOne.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#SBATCH --time=1:00:00
-#SBATCH --cpus-per-task 1 
+#SBATCH --time=0:30:00
+#SBATCH --cpus-per-task 2 
+#SBATCH --mem=8192
+#SBATCH --qos=preempt
 
 logPath=$1
 if [ -z "$SLURM_ARRAY_TASK_ID" ]; then
@@ -16,9 +18,13 @@ if [ `whoami` == "martin" ]; then
   export PATH=/work/R-3.1.1/bin:$PATH
   export R_LIBS=/work/R-3.1.1/library/
 else
-  module add apps/R/3.1.2
-  export R_LIBS=/home/m/mllofriualon/R-library/
+  module add apps/R/3.3.2
+  export R_LIBS=/home/m/mllofriualon/work/rlib
+  module add apps/jre/1.8.0_121.x86 
+  module unload apps/jre/1.7.0_80.x64
 fi
+module list
+java -version
 
-/usr/bin/java -cp "./platform/src/:./multiscalemodel/src/:./bin/:./deps/*:./deps/j3dport/*" edu.usf.experiment.RunIndividualByNumber $logPath $individual
+java -cp "./platform/src/:./multiscalemodel/src/:./bin/:./deps/*:./deps/j3dport/*" edu.usf.experiment.RunIndividualByNumber $logPath $individual
 
