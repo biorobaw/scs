5f68ba3d80734b886b3d5ee99885807839149eb7
Martin Llofriu <mllofriu@gmail.com>
2017-05-31 13:03:15 -0400

    Increase wall thickness

--- platform/src/edu/usf/experiment/display/drawer/WallDrawer.java
+++ platform/src/edu/usf/experiment/display/drawer/WallDrawer.java
@@ -1,8 +1,11 @@
 package edu.usf.experiment.display.drawer;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Stroke;
 
 import edu.usf.experiment.universe.wall.Wall;
 import edu.usf.experiment.universe.wall.WallUniverse;
@@ -18,11 +21,15 @@ public class WallDrawer implements Drawer {
 	@Override
 	public void draw(Graphics g, Scaler s) {
 		g.setColor(Color.BLACK);
+		Graphics2D g2d = (Graphics2D)g;
+		Stroke oldStroke = g2d.getStroke();
+		g2d.setStroke(new BasicStroke(3));
 		for (Wall w : u.getWalls()){
 			Point p0 = s.scale(w.s.p0);
 			Point p1 = s.scale(w.s.p1);
 			g.drawLine(p0.x, p0.y, p1.x, p1.y);
 		}
+		g2d.setStroke(oldStroke);
 	}
 
 }
