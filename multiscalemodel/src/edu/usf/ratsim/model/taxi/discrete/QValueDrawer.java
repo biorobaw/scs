e2cd9f6d0c64e14029b4767862c176550ef177e2
Martin Llofriu <mllofriu@gmail.com>
2017-06-01 12:34:26 -0400

    Autoscale value drawer

--- multiscalemodel/src/edu/usf/ratsim/model/taxi/discrete/QValueDrawer.java
+++ multiscalemodel/src/edu/usf/ratsim/model/taxi/discrete/QValueDrawer.java
@@ -25,12 +25,21 @@ public class QValueDrawer implements Drawer {
 	public void draw(Graphics g, Scaler s) {
 		Map<Point3f, Float> vps = model.getValuePoints();
 		
+		float maxPos = -Float.MAX_VALUE;
+		float maxNeg = Float.MAX_VALUE;
+		for (Float val : vps.values())
+			if (val < 0)
+				maxNeg = val < maxNeg ? val : maxNeg;
+			else 
+				maxPos = val > maxPos ? val : maxPos;
+		maxNeg = -maxNeg;
 		
 		for (Point3f p : vps.keySet())
 		{
 			Point ul = s.scale(new Coordinate(p.x, p.y + 1));
 			Point lr = s.scale(new Coordinate(p.x +1, p.y));
 			float val = vps.get(p);
+			val = val < 0 ? val / maxNeg : val / maxPos;
 			Color b = new Color(val < 0 ? 1 : 0,0,val > 0 ? 1 : 0,Math.min(1, Math.abs(val)));
 			g.setColor(b);
 			
