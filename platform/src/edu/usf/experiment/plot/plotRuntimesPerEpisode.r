314845e7b56508052d4cfd720243fa64b5032983
Martin Llofriu <mllofriu@gmail.com>
2017-06-01 12:36:07 -0400

    Downsample before plotting runtimes

--- platform/src/edu/usf/experiment/plot/plotRuntimesPerEpisode.r
+++ platform/src/edu/usf/experiment/plot/plotRuntimesPerEpisode.r
@@ -4,37 +4,27 @@ require(plyr, quietly = TRUE)
 require(dunn.test, quietly = TRUE)
 require(scales, quietly = TRUE)
 
-#stepsPerSec <- 4
+maxColumns  <- 50
 
 plotArrival <- function(pathData, plotName){
   pathData$group <- factor(pathData$group)
-  #pathData <- pathData[pathData$runtime < 10000,]
-  summarizedRunTimes <- ddply(pathData, .(group, repetition), summarise, sdRT = sd(runtime)/sqrt(length(runtime)), mRT = mean(runtime))
-  #   print(head(summarizedRunTimes))
-  summarizedRunTimes <- ddply(summarizedRunTimes, .(group), summarise, repetition=repetition, mRT=mRT, sdRT=sdRT, runmedian = runmed(mRT, 31))
-  #   print(pathData[1,'trial'])
+  
+  # Subsample data
+  numRepetitions <- max(pathData$repetition)
+  columns <- min(numRepetitions, maxColumns)
+  samples <- ceiling(0:(columns) * (numRepetitions / maxColumns))
+  
+  pathData <- pathData[pathData$repetition %in% samples,]
   p <- ggplot(pathData, aes(x = repetition, y = runtime, group = interaction(group, repetition), fill = group)) 
-  #p <- p + geom_bar(data=summarizedRunTimes, mapping=aes(x=Group, y = mRT), stat='identity')
   p <- p + ylab("Completion time (num. actions)\n") + xlab("\nRepetition") 
-  # p <- p + scale_fill_grey(name="Group",start = .4)
   p <- p + theme_bw() + scale_fill_discrete(guide = guide_legend(title = "Group"))
   p <- p + scale_y_continuous() + scale_x_continuous(breaks= pretty_breaks())
   p <- p + theme(legend.text = element_text(size=20), legend.title = element_text(size=20), text = element_text(size=20)) 
   p <- p + theme(legend.key.height = unit(3,"line"), legend.key.width = unit(3,"line"), legend.position = "right", legend.justification = c(1, 1), legend.background = element_rect(colour = NA, fill = NA))
-  box <- p + geom_boxplot(position="dodge")# + geom_jitter()
-  #box <- p + geom_point(aes(color=group), size = 5)
+  box <- p + geom_boxplot(position="dodge", notch = TRUE)# + geom_jitter()
   print(box)
   ggsave(plot=box,filename=paste(plotName, "box.", pathData[1,'trial'],".pdf", sep=''), width=30, height=10)
-  
-  p <- ggplot(summarizedRunTimes, aes(x=group, y = mRT)) 
-  p <- p + ylab("Completion time (s)") + xlab("Group") 
-  p <- p + theme(legend.text = element_text(size=20), legend.title = element_text(size=20), text = element_text(size=20)) 
-  p <- p + theme(legend.key.height = unit(3,"line"), legend.key.width = unit(3,"line"), legend.position = "right", legend.justification = c(1, 1), legend.background = element_rect(colour = NA, fill = NA))
-  p <- p + geom_bar(aes(y=mRT,fill=group),position=position_dodge(1), stat="identity")
-  p <- p + geom_errorbar(aes(x=group, ymax=mRT+sdRT, ymin=mRT-sdRT, color=group, width = 0.25))
-  p <- p + scale_fill_grey(name="Group")
-  p <- p + scale_color_grey(name="Group")
-  ggsave(plot=p,filename=paste(plotName, "bar.", pathData[1,'trial'],".pdf", sep=''), width=10, height=10)
+
 }
 
 # Plot runtimes per episode
