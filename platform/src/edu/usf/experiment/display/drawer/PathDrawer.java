3a585b017247b2394097d884792b5d32ddc3a389
Martin Llofriu <mllofriu@gmail.com>
2017-07-06 20:12:32 -0400

    draw path

---
+++ platform/src/edu/usf/experiment/display/drawer/PathDrawer.java
@@ -0,0 +1,49 @@
+package edu.usf.experiment.display.drawer;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.LinkedList;
+
+import javax.vecmath.Point3f;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+import edu.usf.experiment.robot.LocalizableRobot;
+
+public class PathDrawer implements Drawer {
+
+	private LocalizableRobot robot;
+	private LinkedList<Point> poses;
+
+	public PathDrawer(LocalizableRobot robot){
+		this.robot = robot;
+		
+		poses = new LinkedList<Point>();
+	}
+
+	@Override
+	public void draw(Graphics g, Scaler s) {
+		Point3f pos = robot.getPosition();
+		
+		float centerx = pos.x;
+		float centery = pos.y;
+		Point scaledPos = s.scale(new Coordinate(centerx, centery));
+		poses.add(scaledPos);
+		
+		g.setColor(Color.DARK_GRAY);
+		Point start = poses.get(0);
+		for (int i = 1; i < poses.size(); i++){
+			Point end = poses.get(i);
+			g.drawLine(start.x, start.y, end.x, end.y);
+			start = end;
+		}
+			
+	}
+	
+	@Override
+	public void clearState() {
+		poses.clear();
+	}
+
+}
